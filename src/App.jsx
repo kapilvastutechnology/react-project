ff2f4e44ebdbd2c231b96490cddf0571304c6419
kapilvastutechnology <kapilvastutechnology81@gmail.com>
2025-08-29 15:33:33 +0545

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,45 @@
+import { 
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+ } from "./components/ui/alert-dialog"
+
+
+ import { Button } from "./components/ui/button"
 
 export default function App() {
   return (
+    <>
     <div>
-      <h1>this is main page</h1>
+      <h1>this is hooks project</h1>
+      <AlertDialog>
+  <AlertDialogTrigger>Open</AlertDialogTrigger>
+  <AlertDialogContent>
+    <AlertDialogHeader>
+      <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+      <AlertDialogDescription>
+        This action cannot be undone. This will permanently delete your account
+        and remove your data from our servers.
+      </AlertDialogDescription>
+    </AlertDialogHeader>
+    <AlertDialogFooter>
+      <AlertDialogCancel>Cancel</AlertDialogCancel>
+      <AlertDialogAction>Continue</AlertDialogAction>
+    </AlertDialogFooter>
+  </AlertDialogContent>
+</AlertDialog>
     </div>
+
+    <div className="mt-5">
+      <Button>add</Button>
+    </div>
+    </>
+    
   )
 }
